33872739ff743afc88b5d1ce3bb767c126ae0a55
DominicMytych <dominicmytych@gmail.com>
2022-07-10 23:46:45 +0200

    commit

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+TextStyle kTaskStyle = TextStyle(
+  fontFamily: 'Quicksand',
+  fontSize: 22.0,
+);
+
+TextStyle kHeadingStyle = TextStyle(
+  fontFamily: 'Quicksand',
+  fontSize: 35.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+TextStyle kSubStyle = TextStyle(
+  fontFamily: 'Quicksand',
+  fontSize: 20.0,
+  fontWeight: FontWeight.w800,
+  color: Colors.white,
+);
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/constants.dart';
+import 'package:todo/components/custom_button.dart';
+import 'package:todo/components/task_list.dart';
+import 'package:todo/models/task_data.dart';
+
+import 'add_task_screen.dart';
+
+class TasksScreen extends StatefulWidget {
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.deepOrangeAccent
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 10.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                        ),
+
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                            'TO DO',
+                            style: kHeadingStyle,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            '${Provider.of<TaskData>(context).taskLength()} Tasks',
+                            style: kSubStyle,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white54,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: TaskListView(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 20.0,
+                              bottom: 20.0,
+                              top: 20.0,
+                            ),
+                            child: CustomAddButton(
+                              icon: Icons.add,
+                              ontap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  builder: (context) {
+                                    return SingleChildScrollView(
+                                      child: Container(
+                                        color: Color(0xff1d5a76),
+                                        padding: EdgeInsets.only(
+                                            bottom: MediaQuery.of(context)
+                                                .viewInsets
+                                                .bottom),
+                                        child: AddTaskScreen(),
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
